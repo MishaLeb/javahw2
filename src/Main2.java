6a3d208607036e59010ba63352307aceca98548d
Mike Lebedev <ley3@yandex.ru>
2023-05-13 17:16:39 +0300

    cheange

--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,6 @@
-public class Main {
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main2 {
     public static void main(String[] args) {
 
         int eaters = 5; // сколько людей будут есть
